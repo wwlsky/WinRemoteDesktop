c4d1ec91fa5bcef639a52239d5678af59a75a0c1
Mr.Liang <wwl_sky@hotmail.com>
2018-04-25 11:45:28 +0800

    Update

--- WinRemoteDesktop/FormMain.Designer.cs
+++ WinRemoteDesktop/FormMain.Designer.cs
@@ -279,7 +279,7 @@
             // 
             this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip2;
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Text = "简易远程桌面工具";
             this.notifyIcon1.Visible = true;
             this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
             // 
